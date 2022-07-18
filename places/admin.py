bb590214734d931fc393e070d48166964d60c32a
Alexander <samokhin@shockland.ru>
2022-07-18 17:43:54 +0300

    Add inlines in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Places, Images
 
-# Register your models here.
-admin.site.register(Places)
-admin.site.register(Images)
+class ImageInline(admin.TabularInline):
+    model = Images
+
+class PlacesAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+
+admin.site.register(Places, PlacesAdmin)
